d11b24226c4045cff77fce3ecf76004c2be84c78
Jensen Tonne <jstonne@stackideas.com>
2013-12-18 15:09:17 +0800

    Move exports into amd.js.

--- src/exports/amd.js
+++ src/exports/amd.js
@@ -2,6 +2,40 @@ define([
 	"../core"
 ], function( jQuery ) {
 
+jQuery.version = "$FOUNDRY_VERSION";
+jQuery.long_version = "$FOUNDRY_LONG_VERSION";
+
+jQuery.uid = function(p,s) {
+	return ((p) ? p : "") + Math.random().toString().replace(".","") + ((s) ? s : "");
+};
+
+jQuery.globalNamespace = jQuery.uid("Foundry");
+
+window[jQuery.globalNamespace] = jQuery;
+
+jQuery.run = function(command) {
+	return (jQuery.isFunction(command)) ? command(jQuery) : null;
+};
+
+jQuery.initialize = function(options) {
+
+	if (jQuery.initialized) return;
+
+	jQuery.extend(jQuery, options);
+
+	if (jQuery.environment=="development") {
+		window.F = jQuery;
+	}
+
+	jQuery.initialized = true;
+
+	// Execute any pending modules
+	$BOOTCODE.module.execute();
+}
+
+// Register jquery into bootloader
+$BOOTCODE.jquery(jQuery);
+
 // Register as a named AMD module, since jQuery can be concatenated with other
 // files that may use define, but not via a proper concatenation script that
 // understands anonymous AMD modules. A named AMD is safest and most robust
